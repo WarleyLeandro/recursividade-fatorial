81b004725b77038daff344258292ffff23bac2b7
Warley Leandro <warley.leandro95@gmail.com>
2020-02-16 17:40:01 -0300

    versao completa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,26 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Digite um número: ");
+		int num = input.nextInt();
+		System.out.println("fatorial de "+ num +" é: "+ fat(num));
+		
+		input.close();
+	}
+	
+	//fat of a number
+	private static int fat(int n) {
+		//stop
+		if(n==0) {
+			return 1;
+		}else {
+			return n * fat(n-1);
+		}		
 	}
 
 }
